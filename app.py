86374ea51f471de87ec9821d4bf606160eb19afe
unknown <saurabhgupta989@gmail.com>
2019-11-10 00:04:57 +0530

    initial code for movie trend

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+
+
+@app.route('/signUp', methods=['POST'])
+def signUp():
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+
+    if _name and _email and _password:
+        return "saved"
+    else:
+        return "Enter the required fields"
+
+        if __name__ == '__main__':
+            app.run()
